a1716da320efd561e676e2732b5ebef1795e75cd
exam deboggage et test unitaire <2479247@cegepoutaouais.qc.ca>
2025-10-16 11:45:41 -0400

    version finale

--- notation.py
+++ notation.py
@@ -8,12 +8,12 @@ def valider_notes(notes: list[float]) -> bool:
     :param notes: liste de notes
     :returns: vrai si la liste et valide, sinon faux.
     """
-    if len(notes) < 9:
+    if len(notes) !=9:  #la liste doit contenir exactement 9 entiers.
         return False
 
     for n in notes:
-        if n > 3:
-            return False
+        if n <= 3 and n<=-3: #la liste des notes ne doit pas aussi être plus grand que -3
+            return True
 
     return True
 
@@ -32,10 +32,12 @@ def calculer_points(vbase: float, notes: list[float]) -> float:
         note_min = min(notes)
 
         for i in range(len(notes)):
-            if notes[i] == note_max or note_min:
+            if notes[i] == note_max : #trouver la note max et min pour ensuite la retitrer de la liste
                 notes.remove(notes[i])
+            if notes[i] ==note_min:
+                notes.remove(notes[i]) #j'ai divisé le code en 2 parties pour que ca soit plus claire pour moi.
 
-        moyenne = sum(notes) / 9
+        moyenne = sum(notes) / 7 # la moyenne des notes est = au nombres restant apres avoir rétirer le min et le max
         total = vbase + moyenne
         return total
 
--- test_notation.py
+++ test_notation.py
@@ -2,11 +2,19 @@
 # Tests unitaires pour le module notation.py
 
 import pytest
-from notation import *
+from notation import*
 
 # -----------------------------
 # TODO : Tests unitaires pour valider_notes()
 # -----------------------------
+def test_valider_notes():
+    #arrange
+    notes=[3,2,1,2,3,3,2,2,3]
+    resultat_attendu = True
+    #act
+    resultat_obtenu = valider_notes(notes)
+    #assert
+    assert resultat_attendu == resultat_obtenu
 
 
 
@@ -14,5 +22,17 @@ from notation import *
 # -----------------------------
 # TODO : Tests unitaires pour calculer_points()
 # -----------------------------
+def test_calculer_points():
+    #arrange
+    vbase= 3.2
+    notes = [3,2,1,2,3,3,2,2,3]
+    resultat_attendu = 5.63
+
+    #act
+    resultat_obtenu = calculer_points(vbase,notes)
+
+    #assert
+    assert resultat_attendu == resultat_obtenu
+
 
 
